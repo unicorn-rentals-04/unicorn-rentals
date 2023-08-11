584ef0d0cb1deb236a98b71a301a30ffccfddc07
ipcrm <matthew.cadorette@lacework.net>
2023-08-11 17:29:31 -0400

    feat(db): generate db creds

--- deploy/resources/tf/shared/main.tf
+++ deploy/resources/tf/shared/main.tf
@@ -4,6 +4,26 @@ resource "random_string" "token" {
   upper   = true
 }
 
+resource "random_string" "db_user" {
+  length  = 20
+  special = true
+  upper   = true
+}
+
+resource "random_string" "db_pass" {
+  length  = 20
+  special = true
+  upper   = true
+}
+
 output "auth_token" {
   value = random_string.token.result
+}
+
+output "db_user" {
+  value = random_string.db_user.result
+}
+
+output "db_pass" {
+  value = random_string.db_pass.result
 }
